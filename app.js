db56a486d1932fad6dd42a705466427a3c571eea
Valeria Serci <balerty84@gmail.com>
2021-03-10 20:26:40 +0000

    add a comment

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ const celsius = kelvin - 273;
 //equation to calculate Fahrenheit
 let fahrenheit = celsius * (9/5) + 32;
 
+// round down the result using a method of the Math class
 fahrenheith = Math.floor(fahrenheit);
+
