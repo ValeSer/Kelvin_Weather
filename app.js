3086a4c68bc6230cd1a9f99fcbdb9f4c315e0fd2
Valeria Serci <balerty84@gmail.com>
2021-03-11 19:40:31 +0000

    add newton variable and log it to console

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //The forecast today is 293 Kelvin
-const kelvin = 0;
+const kelvin = 293;
 
 //Celsius is 273 degrees less than Kelvin
 const celsius = kelvin - 273;
 
 //equation to calculate Fahrenheit
-let fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9/5) + 32
 
 // round down the result using a method of the Math class
 fahrenheith = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahreinheit.`);
 
-console.log(`The temperature is ${fahrenheit} degrees Fahreinheit.`)
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`)
